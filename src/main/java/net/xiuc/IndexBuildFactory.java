5d79c9c00bb19a5fcccc6761281e9191bb1a520b
xiuc <972994718@qq.com>
2016-04-27 22:14:30 +0800

    db translate

--- src/main/java/net/xiuc/IndexBuildFactory.java
+++ src/main/java/net/xiuc/IndexBuildFactory.java
@@ -1,5 +1,8 @@
 package net.xiuc;
 
+import net.xiuc.mapping.DbTranslate;
+import net.xiuc.mapping.Translate;
+
 /**
  * 索引构建入口
  *
@@ -15,9 +18,11 @@ public enum IndexBuildFactory {
 
     public <T> void build(String indexAliasName, Class<T> clazz){
         //解析clazz, 分析出主从关系以及数据库关系
-
+        Translate translate = new DbTranslate();
         //从数据库获取数据并建成doc
+        //setting设置
         //创建索引并设置别名
+
         //索引管理
     }
 }
